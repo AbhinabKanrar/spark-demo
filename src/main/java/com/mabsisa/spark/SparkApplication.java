d8ca28528b6a9b776c8eb9948bc5176d64eaccd9
abhinab <abhinabkanrar@gmail.com>
2018-02-17 19:40:44 +0530

    initial commit

--- src/main/java/com/mabsisa/spark/SparkApplication.java
+++ src/main/java/com/mabsisa/spark/SparkApplication.java
@@ -1,8 +1,33 @@
 package com.mabsisa.spark;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
 public class SparkApplication {
 
-	public static void main(String[] args) {
+	private static final Pattern SPACE = Pattern.compile(" ");
 
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount").setMaster("local");
+		JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+		JavaRDD<String> lines = ctx.textFile("/home/abhinab/git/spark-demo/src/main/resources/doc.txt", 1);
+
+		JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+		JavaPairRDD<String, Integer> wordAsTuple = words.mapToPair(word -> new Tuple2<>(word, 1));
+		JavaPairRDD<String, Integer> wordWithCount = wordAsTuple.reduceByKey((Integer i1, Integer i2) -> i1 + i2);
+		List<Tuple2<String, Integer>> output = wordWithCount.collect();
+		for (Tuple2<?, ?> tuple : output) {
+			System.out.println(tuple._1() + ": " + tuple._2());
+		}
+		ctx.stop();
 	}
 }
